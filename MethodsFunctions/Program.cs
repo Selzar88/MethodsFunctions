2ab3aa029b1005805dd3b8e3666c9c9d6842b10f
wisni <wisni@LAPTOP-7AN5K9RK>
2022-06-04 14:27:11 +0100

    Add project files.

---
+++ MethodsFunctions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MethodsFunctions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteSomething();
+            WriteSomethingSpecific("I am an argument and am called from method");
+            Console.Read();
+        }
+        // access modifier (static) return type methode name (parameter1, parameter2)
+        public static void WriteSomething()
+        {
+            Console.WriteLine("I am called from a method");
+            
+        }
+
+        public static void WriteSomethingSpecific(string myText)
+        {
+            Console.WriteLine(myText);
+            
+        }
+    }
+}
